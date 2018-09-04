f4209e8bd1dacb44978cb52c5753dfa468b7c385
rishavroy97 <rishavroy008@gmail.com>
2018-09-05 00:10:02 +0530

    refactored donations.js

--- env.js
+++ env.js
@@ -3,6 +3,9 @@ var pwd = "innovation";
 var smtp = "smtp.gmail.com"; //If the email is gmail , else enter that smtp
 var API_KEY = "483fcdc4c4e13fc1762df91d97a1546a";
 var AUTH_KEY = "5fd27ffe5579cba18b089307450dab94";
+// var API_KEY = "test_4275c7966eb92dad9efd0256465";
+// var AUTH_KEY = "test_31d9a44a553fef27e164d1bf4a5";
+
 exports.email = email;
 exports.pwd = pwd;
 exports.smtp = smtp;

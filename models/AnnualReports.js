64cfdd4a20546c344919ee6ff9a05caffb8ae8a1
rishavroy97 <rishavroy008@gmail.com>
2018-09-29 16:57:01 +0530

    Created the Annual reports model

---
+++ models/AnnualReports.js
@@ -0,0 +1,29 @@
+"use strict"
+module.exports=function(sequelize,DataTypes){
+    var AnnualReports = sequelize.define("AnnualReports",{
+        id:{
+            type:DataTypes.INTEGER,
+            unique:true,
+            autoIncrement:true,
+            allowNull:false,
+            primaryKey:true
+        },
+        fileName:{
+            type:DataTypes.STRING,
+            unique: true,
+            allowNull: false
+        },
+        year:{
+            type:DataTypes.INTEGER,
+            len:[1, 4],
+            allowNull: false
+        },
+        fileLocation: {
+            type: DataTypes.STRING,
+            // allowNull: false
+        }
+    },{
+    freezeTableName: true
+  });
+    return AnnualReports;
+};

19cbe43dac9a1e408bfdf5b2f04614bea1aecba1
Samsung-Blue <sreelakshmias97@gmail.com>
2018-10-28 18:00:41 +0530

    Add faq questions and change datatype for image name field

--- models/Gallery.js
+++ models/Gallery.js
@@ -14,7 +14,7 @@ module.exports=function(sequelize,DataTypes){
             allowNull: false
         },
         image: {
-            type: DataTypes.STRING,
+            type: DataTypes.TEXT,
             allowNull: false,
         }
     },{

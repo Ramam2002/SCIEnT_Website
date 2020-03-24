d8f01aeab167ebf8ff540414b31b4359b99b77bd
AnjaneyaTripathi <anjaneya532000@gmail.com>
2020-03-24 19:19:21 +0530

    rendering of halls done as well as making the dropdown date dynamic

--- public/js/adminPanelHallSummary.js
+++ public/js/adminPanelHallSummary.js
@@ -4,6 +4,9 @@ $(document).ready(function() {
     var month = document.getElementById("months").value;
     var year = document.getElementById("years").value;
     console.log(month + year);
+    $("#hallBookingSummary")
+      .find("tr:gt(1)")
+      .remove();
     $.ajax({
       url: "/admin/getHallSummary",
       method: "POST",
@@ -27,119 +30,14 @@ $(document).ready(function() {
           var D = DATE.getDate();
           var M = DATE.getMonth();
           var Y = DATE.getUTCFullYear();
-          console.log(
-            "Month:month" + m + ":" + month + "/" + "Year:year" + y + ":" + year
-          );
+          var rowCount = $("#hS tr").length;
+          console.log(rowCount);
+          //console.log(
+          //  "Month:month" + m + ":" + month + "/" + "Year:year" + y + ":" + year
+          //);
           if (month == "0" && year == "0") {
-            console.log(item.name);
-            $("#infoList").append(
-              '<div><a href="#"><span class="bookingId">' +
-                item.id +
-                '</span></a><p ><span class="applicantRoll">' +
-                item.roll +
-                " " +
-                '</span><span class="applicantName">' +
-                item.name +
-                '</span></p><p ><span class="applicantDate">' +
-                d +
-                "/" +
-                (m + 1) +
-                "/" +
-                y +
-                " " +
-                '</span><span class="applicantRequestedStartTime">' +
-                item.startTime +
-                '</span></p><p ><span class="applicantDate">' +
-                D +
-                "/" +
-                (M + 1) +
-                "/" +
-                Y +
-                " " +
-                '</span><span class="applicantRequestedEndTime">' +
-                item.endTime +
-                '</span></p><P ><span class="applicantRequestedEndTime">' +
-                item.hallnumber +
-                "</span></p></div>"
-            );
-            showModal();
-          } else if (month == "0") {
-            if (y == year) {
-              console.log(item.name);
-              $("#infoList").append(
-                '<div><a href="#"><span class="bookingId">' +
-                  item.id +
-                  '</span></a><p ><span class="applicantRoll">' +
-                  item.roll +
-                  " " +
-                  '</span><span class="applicantName">' +
-                  item.name +
-                  '</span></p><p ><span class="applicantDate">' +
-                  d +
-                  "/" +
-                  (m + 1) +
-                  "/" +
-                  y +
-                  " " +
-                  '</span><span class="applicantRequestedStartTime">' +
-                  item.startTime +
-                  '</span></p><p ><span class="applicantDate">' +
-                  D +
-                  "/" +
-                  (M + 1) +
-                  "/" +
-                  Y +
-                  " " +
-                  '</span><span class="applicantRequestedEndTime">' +
-                  item.endTime +
-                  '</span></p><P ><span class="applicantRequestedEndTime">' +
-                  item.hallnumber +
-                  "</span></p></div>"
-              );
-              showModal();
-            } else {
-              x++;
-            }
-          } else if (year == "0") {
-            if (m === month - 1) {
-              console.log(item.name);
-              $("#infoList").append(
-                '<div><a href="#"><span class="bookingId">' +
-                  item.id +
-                  '</span></a><p ><span class="applicantRoll">' +
-                  item.roll +
-                  " " +
-                  '</span><span class="applicantName">' +
-                  item.name +
-                  '</span></p><p ><span class="applicantDate">' +
-                  d +
-                  "/" +
-                  (m + 1) +
-                  "/" +
-                  y +
-                  " " +
-                  '</span><span class="applicantRequestedStartTime">' +
-                  item.startTime +
-                  '</span></p><p ><span class="applicantDate">' +
-                  D +
-                  "/" +
-                  (M + 1) +
-                  "/" +
-                  Y +
-                  " " +
-                  '</span><span class="applicantRequestedEndTime">' +
-                  item.endTime +
-                  '</span></p><P ><span class="applicantRequestedEndTime">' +
-                  item.hallnumber +
-                  "</span></p></div>"
-              );
-              showModal();
-            } else {
-              x++;
-            }
-          } else if (year == y && m == month - 1) {
-            console.log("MATCH" + item.name);
-            $("#hallBookingSummary").append(
+            //console.log(item.name);
+            $("#hS").append(
               "<tr>" +
                 "<td >" +
                 '<a href="#">' +
@@ -192,14 +90,186 @@ $(document).ready(function() {
                 "</td>" +
                 "</tr>"
             );
-            //showModal();
+          } else if (month == "0") {
+            if (y == year) {
+              //console.log(item.name);
+              $("#hS").append(
+                "<tr>" +
+                  "<td >" +
+                  '<a href="#">' +
+                  '<span class="bookingId">' +
+                  item.id +
+                  "</span>" +
+                  "</a>" +
+                  "</td>" +
+                  "<td >" +
+                  '<span class="applicantRoll">' +
+                  item.roll +
+                  "</span>" +
+                  "</td>" +
+                  "<td >" +
+                  '<span class="applicantName">' +
+                  item.name +
+                  "</span>" +
+                  "</td>" +
+                  "<td >" +
+                  '<span class="applicantDate">' +
+                  d +
+                  "/" +
+                  (m + 1) +
+                  "/" +
+                  y +
+                  "</span>" +
+                  "</td>" +
+                  "<td >" +
+                  '<span class="applicantRequestedStartTime">' +
+                  item.startTime +
+                  "</span></td>" +
+                  "<td >" +
+                  '<span class="applicantDate">' +
+                  D +
+                  "/" +
+                  (M + 1) +
+                  "/" +
+                  Y +
+                  "</span>" +
+                  "</td>" +
+                  "<td >" +
+                  '<span class="applicantRequestedEndTime">' +
+                  item.endTime +
+                  "</span>" +
+                  "</td>" +
+                  "<td >" +
+                  '<span class="applicantRequestedEndTime">' +
+                  item.hallnumber +
+                  "</span>" +
+                  "</td>" +
+                  "</tr>"
+              );
+            } else {
+              x++;
+            }
+          } else if (year == "0") {
+            if (m === month - 1) {
+              //console.log(item.name);
+              $("#hS").append(
+                "<tr>" +
+                  "<td >" +
+                  '<a href="#">' +
+                  '<span class="bookingId">' +
+                  item.id +
+                  "</span>" +
+                  "</a>" +
+                  "</td>" +
+                  "<td >" +
+                  '<span class="applicantRoll">' +
+                  item.roll +
+                  "</span>" +
+                  "</td>" +
+                  "<td >" +
+                  '<span class="applicantName">' +
+                  item.name +
+                  "</span>" +
+                  "</td>" +
+                  "<td >" +
+                  '<span class="applicantDate">' +
+                  d +
+                  "/" +
+                  (m + 1) +
+                  "/" +
+                  y +
+                  "</span>" +
+                  "</td>" +
+                  "<td >" +
+                  '<span class="applicantRequestedStartTime">' +
+                  item.startTime +
+                  "</span></td>" +
+                  "<td >" +
+                  '<span class="applicantDate">' +
+                  D +
+                  "/" +
+                  (M + 1) +
+                  "/" +
+                  Y +
+                  "</span>" +
+                  "</td>" +
+                  "<td >" +
+                  '<span class="applicantRequestedEndTime">' +
+                  item.endTime +
+                  "</span>" +
+                  "</td>" +
+                  "<td >" +
+                  '<span class="applicantRequestedEndTime">' +
+                  item.hallnumber +
+                  "</span>" +
+                  "</td>" +
+                  "</tr>"
+              );
+            } else {
+              x++;
+            }
+          } else if (year == y && m == month - 1) {
+            //console.log("MATCH" + item.name);
+            $("#hS").append(
+              "<tr>" +
+                "<td >" +
+                '<a href="#">' +
+                '<span class="bookingId">' +
+                item.id +
+                "</span>" +
+                "</a>" +
+                "</td>" +
+                "<td >" +
+                '<span class="applicantRoll">' +
+                item.roll +
+                "</span>" +
+                "</td>" +
+                "<td >" +
+                '<span class="applicantName">' +
+                item.name +
+                "</span>" +
+                "</td>" +
+                "<td >" +
+                '<span class="applicantDate">' +
+                d +
+                "/" +
+                (m + 1) +
+                "/" +
+                y +
+                "</span>" +
+                "</td>" +
+                "<td >" +
+                '<span class="applicantRequestedStartTime">' +
+                item.startTime +
+                "</span></td>" +
+                "<td >" +
+                '<span class="applicantDate">' +
+                D +
+                "/" +
+                (M + 1) +
+                "/" +
+                Y +
+                "</span>" +
+                "</td>" +
+                "<td >" +
+                '<span class="applicantRequestedEndTime">' +
+                item.endTime +
+                "</span>" +
+                "</td>" +
+                "<td >" +
+                '<span class="applicantRequestedEndTime">' +
+                item.hallnumber +
+                "</span>" +
+                "</td>" +
+                "</tr>"
+            );
           } else {
             x++;
-            console.log("Babaji ka Thullu!" + x);
+            //console.log("Babaji ka Thullu!" + x);
           }
           console.log(x + "//" + z);
           if (x === z) {
-            console.log("POPAT");
+            //console.log("POPAT");
             $("#infoList").append(
               "<h2>NO ROOM BOOKED FOR THE SELECTED TIME DURATION</h2>"
             );
